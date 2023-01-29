51c9ef8fd38641fbb1e540d68fe2962b20256386
KesterDaniel <kesterdan17@gmail.com>
2023-01-29 22:44:48 +0100

    getting the Squirrel number sum of each dataframe

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ Black_Squirrels = data[data["Primary Fur Color"] == "Black"]
 Gray_Squirrels = data[data["Primary Fur Color"] == "Gray"]
 Cinnamon_Squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
 
+Num_Black_Squirrels = sum(Black_Squirrels["Hectare Squirrel Number"])
+Num_Gray_Squirrels = sum(Gray_Squirrels["Hectare Squirrel Number"])
+Num_Cinnamon_Squirrels = sum(Cinnamon_Squirrels["Hectare Squirrel Number"])
+
 
