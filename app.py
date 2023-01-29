47ee21a1f1cdfab09dfa58b5fcb780edbb821ba6
KesterDaniel <kesterdan17@gmail.com>
2023-01-29 22:38:58 +0100

    grouping all squirrels according to color

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 import pandas
+
+data = pandas.read_csv("Squirrel Census Data.csv")
+
+Black_Squirrels = data[data["Primary Fur Color"] == "Black"]
+Gray_Squirrels = data[data["Primary Fur Color"] == "Gray"]
+Cinnamon_Squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
+
+
